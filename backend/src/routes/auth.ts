165aabc5f934608f529ebd9cf287350b8830caf8
10-abhi <abhishek81936@gmail.com>
2025-07-28 19:17:16 +0530

    updated auth with typeorm

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { google } from "googleapis";
-import { prismaDB } from "../lib/prisma";
+import { AppDataSource } from "../data-source";
+import { User } from "../entity/User";
+import { v4 as uuidv4 } from "uuid";
 
 const auther = Router();
 
@@ -13,7 +15,7 @@ export const oath2client = new google.auth.OAuth2({
 //redirect to google login
 auther.get("/google", (req, res) => {
     const scopes = [
-        "https://www.googleapis.com/auth/drive.metadata.readonly",
+        "https://www.googleapis.com/auth/drive",
         "https://www.googleapis.com/auth/userinfo.email",
         "https://www.googleapis.com/auth/userinfo.profile",
     ];
@@ -37,40 +39,75 @@ auther.get("/google/callback", async (req, res) => {
             auth: oath2client, version: "v2"
         });
         const { data: userinfo } = await oauth2.userinfo.get();
-        if (!userinfo.email|| !userinfo.name || !userinfo.id || !tokens.access_token || !tokens.refresh_token) {
+        if (!userinfo.email || !userinfo.name || !userinfo.id || !tokens.access_token || !tokens.refresh_token) {
             return res.status(400).json({
                 error: "Missing Google User ID || access token || refresh token"
             })
         }
-        await prismaDB.user.upsert({
-            where: {
-                googleUserID: userinfo.id,
-            },
-            update: {
-                name: userinfo.name,
-                email: userinfo.email,
-                accessToken: tokens.access_token,
-                refreshToken: tokens.refresh_token
-            },
-            create: {
-                googleUserID: userinfo.id,
-                name: userinfo.id,
-                email: userinfo.email,
-                accessToken: tokens.access_token!,
-                refreshToken: tokens.refresh_token!,
-            }
-        });
+        const sessionToken = uuidv4();
+        const userRepo = AppDataSource.getRepository(User);
+        await userRepo.upsert({
+            googleUserID: userinfo.id,
+            name: userinfo.name,
+            email: userinfo.email,
+            accessToken: tokens.access_token,
+            refreshToken: tokens.refresh_token
+        },
+            ['googleUserID']
+        );
+
+        // await prismaDB.user.upsert({
+        //     where: {
+        //         googleUserID: userinfo.id,
+        //     },
+        //     update: {
+        //         name: userinfo.name,
+        //         email: userinfo.email,
+        //         accessToken: tokens.access_token,
+        //         refreshToken: tokens.refresh_token
+        //     },
+        //     create: {
+        //         googleUserID: userinfo.id,
+        //         name: userinfo.id,
+        //         email: userinfo.email,
+        //         accessToken: tokens.access_token!,
+        //         refreshToken: tokens.refresh_token!,
+        //     }
+        // });
 
         // console.log("user info", userinfo);
         // console.log("Tokens", tokens);
         // After successful authentication and getting user info:
-        const frontendSuccessUrl = `http://localhost:3000/auth/success?email=${encodeURIComponent(userinfo.email)}&name=${encodeURIComponent(userinfo.name)}&googleUserID=${encodeURIComponent(userinfo.id)}`;
-        res.redirect(frontendSuccessUrl);
-        res.send("Google Drive connected succ!");
+        res.cookie("user_session", sessionToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            maxAge: 1000 * 60 * 60 * 24,
+        })
+        return res.redirect("http://localhost:3000/auth/success");
     } catch (error) {
         console.error("Oauth error : ", error);
         res.status(500).send("Google authentication failed");
     }
 });
+
+    auther.get("/me" , async(req,res)=>{
+        const sessionToken = req.cookies["user_session"];
+        if(!sessionToken)return res.status(401).json({
+            error : "Unauthenticated"
+        })
+        const userRepo = AppDataSource.getRepository(User);
+        const user = await userRepo.findOne({where : {sessionToken}});
+
+        if(!user)return res.status(401).json({
+            error : "Invalid Session"
+        })
+        return res.json({
+            id : user.id,
+            name : user.name,
+            email: user.email,
+            googleUserID : user.googleUserID
+        });
+    })
 
 export default auther;
