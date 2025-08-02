ba01268a568d15341264c5aa9c30c40dd0f2245b
10-abhi <abhishek81936@gmail.com>
2025-08-02 17:04:05 +0530

    resolved auth bugs

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -2,16 +2,18 @@ import { Router } from "express";
 import { google } from "googleapis";
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
+import { File } from "../entity/File";
 import { v4 as uuidv4 } from "uuid";
 
 const auther = Router();
 
-export const oath2client = new google.auth.OAuth2({
+export const oauth2client = new google.auth.OAuth2({
     client_id: process.env.google_client_id,
     client_secret: process.env.google_client_secret,
     redirectUri: process.env.GOOGLE_REDIRECT_URI
 })
-
+export const userRepo = AppDataSource.getRepository(User);
+export const fileRepo = AppDataSource.getRepository(File);
 //redirect to google login
 auther.get("/google", (req, res) => {
     const scopes = [
@@ -20,7 +22,7 @@ auther.get("/google", (req, res) => {
         "https://www.googleapis.com/auth/userinfo.profile",
     ];
 
-    const authUrl = oath2client.generateAuthUrl({
+    const authUrl = oauth2client.generateAuthUrl({
         access_type: "offline",
         scope: scopes,
         prompt: "consent",
@@ -32,11 +34,10 @@ auther.get("/google", (req, res) => {
 auther.get("/google/callback", async (req, res) => {
     try {
         const code = req.query.code as string;
-        const { tokens } = await oath2client.getToken(code);
-        oath2client.setCredentials(tokens);
-
+        const { tokens } = await oauth2client.getToken(code);
+        oauth2client.setCredentials(tokens);
         const oauth2 = google.oauth2({
-            auth: oath2client, version: "v2"
+            auth: oauth2client, version: "v2"
         });
         const { data: userinfo } = await oauth2.userinfo.get();
         if (!userinfo.email || !userinfo.name || !userinfo.id || !tokens.access_token || !tokens.refresh_token) {
@@ -45,13 +46,14 @@ auther.get("/google/callback", async (req, res) => {
             })
         }
         const sessionToken = uuidv4();
-        const userRepo = AppDataSource.getRepository(User);
         await userRepo.upsert({
             googleUserID: userinfo.id,
             name: userinfo.name,
             email: userinfo.email,
             accessToken: tokens.access_token,
-            refreshToken: tokens.refresh_token
+            refreshToken: tokens.refresh_token,
+            sessionToken:sessionToken,
+            accessTokenExpiresAt : new Date(tokens.expiry_date||Date.now())
         },
             ['googleUserID']
         );
@@ -83,6 +85,7 @@ auther.get("/google/callback", async (req, res) => {
             secure: process.env.NODE_ENV === "production",
             sameSite: "lax",
             maxAge: 1000 * 60 * 60 * 24,
+            // domain:process.env.COOKIE_DOMAIN (if frontend and backend are on diff subdomains)
         })
         return res.redirect("http://localhost:3000/auth/success");
     } catch (error) {
@@ -96,7 +99,6 @@ auther.get("/google/callback", async (req, res) => {
         if(!sessionToken)return res.status(401).json({
             error : "Unauthenticated"
         })
-        const userRepo = AppDataSource.getRepository(User);
         const user = await userRepo.findOne({where : {sessionToken}});
 
         if(!user)return res.status(401).json({
